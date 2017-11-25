e877d0a308eec90c45e07b4e833c15672274fb3b
erukiti <erukiti@gmail.com>
2017-11-25 12:56:31 +0900

    rebass 採用

--- src/renderer/app/component.tsx
+++ src/renderer/app/component.tsx
@@ -3,14 +3,27 @@ import { AppProps } from './index'
 import TopicList from '../topic-list'
 import Working from '../working'
 
+import { Provider, Flex, Box } from 'rebass'
+
+
 export default class AppComponent extends React.Component<AppProps> {
   render() {
+    const theme = {
+      font: '"Noto Sans Japanese"',
+      monospace: 'Ricrty Dimished Discord, Ricty Dimished, "MyricaM M"'
+    }
     return (
-      <div>
-        <TopicList />
-        <hr />
-        <Working />
-      </div>
+      <Provider theme={theme}>
+        <Flex>
+          <Box w={0.8} mx={2}>
+            <Working />
+          </Box>
+          <Box w={0.2} mx={2}>
+            <TopicList />
+            <hr />
+          </Box>
+        </Flex>
+      </Provider>
     )
   }
 }
